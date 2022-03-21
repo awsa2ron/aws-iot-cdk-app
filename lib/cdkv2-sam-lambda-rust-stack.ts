bea7b833d1dc6555f44e09be680f0e36ebd32e23
overheat <aaron.tsui@outlook.com>
2022-03-21 11:14:12 +0800

    named resources.

--- lib/cdkv2-sam-lambda-rust-stack.ts
+++ lib/cdkv2-sam-lambda-rust-stack.ts
@@ -6,13 +6,14 @@ import * as events from 'aws-cdk-lib/aws-lambda-event-sources';
 import * as logs from 'aws-cdk-lib/aws-logs';
 import * as iot from '@aws-cdk/aws-iot-alpha';
 import * as actions from '@aws-cdk/aws-iot-actions-alpha';
+import * as ts from '@aws-cdk/aws-timestream';
 
-const STREAM_NAME = 'iot-cdk-app-stream'
-const STREAM_PARTITION_KEY = 'iot-cdk-app-partition-key'
-const LOG_GROUP_NAME = 'iot-cdk-app-log'
-const IOT_RULE_NAME = 'iot-cdk-app-rule'
+const STREAM_NAME = 'iotCdkAppStream'
+const STREAM_PARTITION_KEY = 'iotCdkAppPartitionkey'
+const LOG_GROUP_NAME = 'iotCdkAppLog'
+const IOT_RULE_NAME = 'iotCdkAppRule'
 const IOT_RULE_SQL = "SELECT * FROM 'test/iotrule'"
-const LAMBDA_NAME = 'iot-cdk-app-lambda'
+const LAMBDA_NAME = 'iotCdkAppLambda'
 const LAMBDA_DESCRIPTION = 'IoT rule to kinesis, then triger a lambda do something'
 const LAMBDA_HANDLER = 'not.required'
 
@@ -21,7 +22,9 @@ export class Cdkv2SamLambdaRustStack extends Stack {
     super(scope, id, props);
 
     // Kinesis data stream
-    let stream = new kinesis.Stream(this, STREAM_NAME);
+    let stream = new kinesis.Stream(this, STREAM_NAME + 'Id', {
+      streamName: STREAM_NAME,
+    });
 
     // AWS IoT rule action
     // Write(PutRecord) to AWS Kinesis Data Stream
@@ -30,17 +33,21 @@ export class Cdkv2SamLambdaRustStack extends Stack {
     });
 
     // AWS CloudWatch log group
-    let logGroup = new logs.LogGroup(this, LOG_GROUP_NAME);
+    let logGroup = new logs.LogGroup(this, LOG_GROUP_NAME + 'Id', {
+      logGroupName: LOG_GROUP_NAME,
+    });
 
     // AWS IoT rule
-    let topicRule = new iot.TopicRule(this, IOT_RULE_NAME, {
+    let topicRule = new iot.TopicRule(this, IOT_RULE_NAME + 'Id', {
+      topicRuleName: IOT_RULE_NAME,
       sql: iot.IotSql.fromStringAsVer20160323(IOT_RULE_SQL),
       errorAction: new actions.CloudWatchLogsAction(logGroup),
     });
     topicRule.addAction(action);
 
     // AWS Lambda function
-    let stream_lambda = new lambda.Function(this, LAMBDA_NAME, {
+    let stream_lambda = new lambda.Function(this, LAMBDA_NAME + 'Id', {
+      functionName: LAMBDA_NAME,
       description: LAMBDA_DESCRIPTION,
       code: lambda.Code.fromAsset(
         'functions/lambda'
@@ -60,5 +67,13 @@ export class Cdkv2SamLambdaRustStack extends Stack {
       startingPosition: lambda.StartingPosition.TRIM_HORIZON,
     });
     stream_lambda.addEventSource(stream_source);
+
+    // const resource = new ts.CfnDatabase(this, DATABASE_NAME, {
+    //   databaseName: DATABASE_NAME,
+    //   kmsKeyId: DATABASE_NAME,
+    //   tags: null,
+    // })
+    // let ts_table = new ts.CfnTable(this, 'Table');
+
   }
 }
--- todo.sh
+++ todo.sh
@@ -5,6 +5,6 @@ cargo build --target x86_64-unknown-linux-musl && \
 cp target/x86_64-unknown-linux-musl/debug/bootstrap lambda && \
 cd .. && \
 cdk synth --no-staging && \
-sam local invoke iot-cdk-app \
+sam local invoke iot-cdk-app-lambda \
 --event test.json \
 --template ./cdk.out/Cdkv2SamLambdaRustStack.template.json
