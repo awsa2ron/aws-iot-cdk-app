60155ce96150072b980b56acd9c7b598dfa5a46b
Aaron Tsui <aaron.tsui@outlook.com>
2022-03-24 16:10:54 +0800

    add DynamoDB list into lambda, and update runtime

--- functions/disconnections/src/main.rs
+++ functions/disconnections/src/main.rs
@@ -1,4 +1,6 @@
+use aws_sdk_dynamodb::Client;
 use lambda_runtime::{service_fn, Error, LambdaEvent};
+
 use serde_json::{json, Value};
 
 #[tokio::main]
@@ -15,13 +17,27 @@ async fn main() -> Result<(), Error> {
 }
 
 pub(crate) async fn func(event: LambdaEvent<Value>) -> Result<Value, Error> {
-  let (event, ctx) = event.into_parts();
-  tracing::info!("{}", event);
-  let message = event["message"].as_str().unwrap_or("world");
-  let first_name = event["firstName"].as_str().unwrap_or("Anonymous");
+  let shared_config = aws_config::load_from_env().await;
+  let client = Client::new(&shared_config);
+  let req = client.list_tables().limit(10);
+  let resp = req.send().await?;
+  tracing::info!("Tables:");
 
-  let response = format!("Hello, {}! Your name is {}", message, first_name);
-  tracing::info!("{}", response);
+  let names = resp.table_names().unwrap_or_default();
 
-  Ok(json!({ "response": response }))
+  for name in names {
+    tracing::info!(" {}", name);
+  }
+
+  Ok(json!({ "response": names }))
+
+  // let (event, ctx) = event.into_parts();
+  // tracing::info!("{}", event);
+  // let message = event["message"].as_str().unwrap_or("world");
+  // let first_name = event["firstName"].as_str().unwrap_or("Anonymous");
+
+  // let response = format!("Hello, {}! Your name is {}", message, first_name);
+  // tracing::info!("{}", response);
+
+  // Ok(json!({ "response": response }))
 }
